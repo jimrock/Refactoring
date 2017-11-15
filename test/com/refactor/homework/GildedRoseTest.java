6d17b586914d6be4081d890fb02bb3b501f5f15c
Jim Chen <jim.chen@cargosmart.com>
2017-11-15 14:52:35 +0800

    [GildeRose]Refacotr_add_first_fail_test

---
+++ test/com/refactor/homework/GildedRoseTest.java
@@ -0,0 +1,12 @@
+package com.refactor.homework;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GildedRoseTest {
+    @Test
+    public void updateQuality() throws Exception {
+        Assert.fail("Not implement!");
+    }
+
+}
