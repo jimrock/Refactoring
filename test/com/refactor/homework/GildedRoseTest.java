24534de0fc5cd1933430e4c3f3018a633a5d2937
Jim Chen <jim.chen@cargosmart.com>
2017-11-18 10:58:11 +0800

    [GildeRose]Refacotr_move_main_function_to_test

--- test/com/refactor/homework/GildedRoseTest.java
+++ test/com/refactor/homework/GildedRoseTest.java
@@ -3,10 +3,26 @@ package com.refactor.homework;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GildedRoseTest {
     @Test
-    public void updateQuality() throws Exception {
+    public void testUpdateQuality() throws Exception {
+
+        GildedRose gildedRose = new GildedRose();
+        System.out.println("OMGHAI!");
+        List<Item> items = new ArrayList<Item>();
+        items.add(new Item("+5 Dexterity Vest", 10, 20));
+        items.add(new Item("Aged Brie", 2, 0));
+        items.add(new Item("Elixir of the Mongoose", 5, 7));
+        items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+        items.add(new Item("Conjured Mana Cake", 3, 6));
+        gildedRose.setItems(items);
+
+        gildedRose.updateQuality();
+
         Assert.fail("Not implement!");
     }
-
 }
