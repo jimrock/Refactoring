1c8a48c8f0c0004edb4b0a034c64f7d266be8558
Jim Chen <jim.chen@cargosmart.com>
2017-11-20 21:31:25 +0800

    [GildeRose]Refactor_Too_Long_Methoed_UpdateQuality

--- src/com/refactor/homework/GildedRose.java
+++ src/com/refactor/homework/GildedRose.java
@@ -15,44 +15,34 @@ public class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < getItems().size(); i++) {
             Item item = getItems().get(i);
-            if (("+5 Dexterity Vest".equals(item.getName())) || "Elixir of the Mongoose".equals(item.getName()) || "Conjured Mana Cake".equals(item.getName())) {
-                if (item.getQuality() > 0) {
-                    item.setQuality(item.getQuality() - 1);
-                }
-            } else {
-                if (item.getQuality() < 50) {
-                    item.setQuality(item.getQuality() + 1);
-
-                    if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
-                        if (item.getSellIn() < 11) {
-                            if (item.getQuality() < 50) {
-                                item.setQuality(item.getQuality() + 1);
-                            }
-                        }
-
-                        if (item.getSellIn() < 6) {
-                            if (item.getQuality() < 50) {
-                                item.setQuality(item.getQuality() + 1);
-                            }
-                        }
-                    }
-                }
-            }
+            updateQuality(item);
             updateSellIn(item);
-
-
             if (item.getSellIn() < 0) {
-                if (("+5 Dexterity Vest".equals(item.getName())) || "Elixir of the Mongoose".equals(item.getName()) || "Conjured Mana Cake".equals(item.getName())) {
-                    if (item.getQuality() > 0) {
-                        item.setQuality(item.getQuality() - 1);
-                    }
-                }
+                updateQualityForNegativeSellIn(item);
+            }
+        }
+    }
+
+    private void updateQuality(Item item) {
+        if (("+5 Dexterity Vest".equals(item.getName())) || "Elixir of the Mongoose".equals(item.getName()) || "Conjured Mana Cake".equals(item.getName())) {
+            if (item.getQuality() > 0) {
+                item.setQuality(item.getQuality() - 1);
+            }
+        } else {
+            if (item.getQuality() < 50) {
+                item.setQuality(item.getQuality() + 1);
+
                 if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
-                    item.setQuality(item.getQuality() - item.getQuality());
-                }
-                if ("Aged Brie".equals(item.getName())) {
-                    if (item.getQuality() < 50) {
-                        item.setQuality(item.getQuality() + 1);
+                    if (item.getSellIn() < 11) {
+                        if (item.getQuality() < 50) {
+                            item.setQuality(item.getQuality() + 1);
+                        }
+                    }
+
+                    if (item.getSellIn() < 6) {
+                        if (item.getQuality() < 50) {
+                            item.setQuality(item.getQuality() + 1);
+                        }
                     }
                 }
             }
@@ -61,6 +51,22 @@ public class GildedRose {
 
     private void updateSellIn(Item item) {
         item.setSellIn("Sulfuras, Hand of Ragnaros".equals(item.getName()) ? item.getSellIn() : item.getSellIn() - 1);
+    }
+
+    private void updateQualityForNegativeSellIn(Item item) {
+        if (("+5 Dexterity Vest".equals(item.getName())) || "Elixir of the Mongoose".equals(item.getName()) || "Conjured Mana Cake".equals(item.getName())) {
+            if (item.getQuality() > 0) {
+                item.setQuality(item.getQuality() - 1);
+            }
+        }
+        if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+            item.setQuality(item.getQuality() - item.getQuality());
+        }
+        if ("Aged Brie".equals(item.getName())) {
+            if (item.getQuality() < 50) {
+                item.setQuality(item.getQuality() + 1);
+            }
+        }
     }
 
     public List<Item> getItems() {
