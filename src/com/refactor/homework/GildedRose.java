cafa494880aed163c2421b9cc1f37dc85e17241f
Jim Chen <jim.chen@cargosmart.com>
2017-11-20 21:46:34 +0800

    [GildeRose]Refactor_Move_update_behavior_to_Item_and_private_attr

--- src/com/refactor/homework/GildedRose.java
+++ src/com/refactor/homework/GildedRose.java
@@ -15,57 +15,7 @@ public class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < getItems().size(); i++) {
             Item item = getItems().get(i);
-            updateQuality(item);
-            updateSellIn(item);
-            if (item.getSellIn() < 0) {
-                updateQualityForNegativeSellIn(item);
-            }
-        }
-    }
-
-    private void updateQuality(Item item) {
-        if (("+5 Dexterity Vest".equals(item.getName())) || "Elixir of the Mongoose".equals(item.getName()) || "Conjured Mana Cake".equals(item.getName())) {
-            if (item.getQuality() > 0) {
-                item.setQuality(item.getQuality() - 1);
-            }
-        } else {
-            if (item.getQuality() < 50) {
-                item.setQuality(item.getQuality() + 1);
-
-                if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
-                    if (item.getSellIn() < 11) {
-                        if (item.getQuality() < 50) {
-                            item.setQuality(item.getQuality() + 1);
-                        }
-                    }
-
-                    if (item.getSellIn() < 6) {
-                        if (item.getQuality() < 50) {
-                            item.setQuality(item.getQuality() + 1);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private void updateSellIn(Item item) {
-        item.setSellIn("Sulfuras, Hand of Ragnaros".equals(item.getName()) ? item.getSellIn() : item.getSellIn() - 1);
-    }
-
-    private void updateQualityForNegativeSellIn(Item item) {
-        if (("+5 Dexterity Vest".equals(item.getName())) || "Elixir of the Mongoose".equals(item.getName()) || "Conjured Mana Cake".equals(item.getName())) {
-            if (item.getQuality() > 0) {
-                item.setQuality(item.getQuality() - 1);
-            }
-        }
-        if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
-            item.setQuality(item.getQuality() - item.getQuality());
-        }
-        if ("Aged Brie".equals(item.getName())) {
-            if (item.getQuality() < 50) {
-                item.setQuality(item.getQuality() + 1);
-            }
+            item.update();
         }
     }
 
--- src/com/refactor/homework/Item.java
+++ src/com/refactor/homework/Item.java
@@ -1,16 +1,16 @@
 package com.refactor.homework;
 
 public class Item {
-    public String name;
-	public int sellIn; 
-    public int quality; 
-    
+    private String name;
+    private int sellIn;
+    private int quality;
+
     public Item(String name, int sellIn, int quality) {
 		this.setName(name);
 		this.setSellIn(sellIn);
 		this.setQuality(quality);
 	}
-    
+
 	/* Generated getter and setter code */
     public String getName() {
 		return name;
@@ -30,4 +30,58 @@ public class Item {
 	public void setQuality(int quality) {
 		this.quality = quality;
 	}
+
+    void updateSellIn() {
+        setSellIn("Sulfuras, Hand of Ragnaros".equals(getName()) ? getSellIn() : getSellIn() - 1);
+    }
+
+    void updateQuality() {
+        if (("+5 Dexterity Vest".equals(getName())) || "Elixir of the Mongoose".equals(getName()) || "Conjured Mana Cake".equals(getName())) {
+            if (getQuality() > 0) {
+                setQuality(getQuality() - 1);
+            }
+        } else {
+            if (getQuality() < 50) {
+                setQuality(getQuality() + 1);
+
+                if ("Backstage passes to a TAFKAL80ETC concert".equals(getName())) {
+                    if (getSellIn() < 11) {
+                        if (getQuality() < 50) {
+                            setQuality(getQuality() + 1);
+                        }
+                    }
+
+                    if (getSellIn() < 6) {
+                        if (getQuality() < 50) {
+                            setQuality(getQuality() + 1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void updateQualityForNegativeSellIn() {
+        if (("+5 Dexterity Vest".equals(getName())) || "Elixir of the Mongoose".equals(getName()) || "Conjured Mana Cake".equals(getName())) {
+            if (getQuality() > 0) {
+                setQuality(getQuality() - 1);
+            }
+        }
+        if ("Backstage passes to a TAFKAL80ETC concert".equals(getName())) {
+            setQuality(getQuality() - getQuality());
+        }
+        if ("Aged Brie".equals(getName())) {
+            if (getQuality() < 50) {
+                setQuality(getQuality() + 1);
+            }
+        }
+    }
+
+    void update() {
+        updateQuality();
+        updateSellIn();
+        if (getSellIn() < 0) {
+            updateQualityForNegativeSellIn();
+        }
+    }
 }
